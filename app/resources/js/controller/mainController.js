1bf171650f0b1226da2fb3db54c34e9560d18b80
avester <runt9ab@gmail.com>
2015-08-10 18:37:41 -0500

    Huge overhaul to get classes in place - Let's actually properly version control this, just remember to fix for prod

--- app/resources/js/controller/mainController.js
+++ app/resources/js/controller/mainController.js
@@ -1,6 +1,9 @@
-var mainController = pfcmApp.controller('mainController', ['$scope', '$http', '$modal', '$router', '$location', function($scope, $http, $modal, $router, $location) {
+pfcmControllers.controller('MainController', MainController);
+
+function MainController($scope, $router, $http, $modal, $location, characterService) {
     // Variables we'll need throughout
-    $scope.characterSelected = false;
+    //TODO $scope.characterSelected = false;
+    $scope.characterSelected = true;
     $scope.characterSelect = "";
     $scope.loading = true;
     $scope.user = {
@@ -8,7 +11,6 @@ var mainController = pfcmApp.controller('mainController', ['$scope', '$http', '$
         username: ''
     };
 
-    // Router config
     $router.config([
         {path: '/statsSkills', component: 'statsSkills'},
         {path: '/equipmentAbilities', component: 'equipmentAbilities'},
@@ -22,7 +24,8 @@ var mainController = pfcmApp.controller('mainController', ['$scope', '$http', '$
         $scope.user.authenticated = true;
         $scope.loading = false;
     }).error(function() {
-        $scope.user.authenticated = false;
+        //TODO $scope.user.authenticated = false;
+        $scope.user.authenticated = true;
         $scope.loading = false;
     });
 
@@ -88,4 +91,86 @@ var mainController = pfcmApp.controller('mainController', ['$scope', '$http', '$
             $scope.loading = false;
         });
     };
-}]);
+
+    $scope.character = new Character({
+        playerName: 'Alex',
+        characterName: 'Ren\'Del Treeborne',
+        alignment: 'Neutral',
+        race: 'Half-Elf',
+        experience: 19800,
+        gender: 'Male',
+        age: 33,
+        height: '6\'6"',
+        weight: 185,
+        hair: 'Black',
+        eyes: 'Green',
+        deity: 'N/A',
+        homeland: 'Srolai Forest',
+        classes: [
+            {id: 1, level: 1}
+        ],
+        attributes: {
+            strength: {score: 16, temp: 0},
+            dexterity: {score: 14, temp: 0},
+            constitution: {score: 10, temp: 0},
+            intelligence: {score: 10, temp: 0},
+            wisdom: {score: 17, temp: 0},
+            charisma: {score: 10, temp: 0}
+        },
+        maxHitPoints: 40,
+        currentHitPoints: 40,
+        tempHitPoints: 0,
+        weapons: [1],
+        skills: {
+            acrobatics: 1,
+            appraise: 0,
+            bluff: 1,
+            climb: 1,
+            craft: 0,
+            diplomacy: 0,
+            disableDevice: 0,
+            disguise: 1,
+            escapeArtist: 0,
+            fly: 0,
+            handleAnimal: 0,
+            heal: 0,
+            intimidate: 1,
+            knArcana: 1,
+            knDungeoneering: 1,
+            knEngineering: 1,
+            knGeography: 0,
+            knHistory: 0,
+            knLocal: 1,
+            knNature: 0,
+            knNobility: 1,
+            knPlanes: 2,
+            knReligion: 0,
+            linguistics: 0,
+            perception: 0,
+            perform: 0,
+            profession: 3,
+            ride: 0,
+            senseMotive: 0,
+            sleightOfHand: 1,
+            spellcraft: 0,
+            stealth: 0,
+            survival: 0,
+            swim: 0,
+            useMagicDevice: 1
+        },
+        equipment: [
+            {qty: 1, item: 1, type: ItemType.ITEM},
+            {qty: 500, item: 2, type: ItemType.ITEM},
+            {qty: 1, item: 1, type: ItemType.ARMOR}
+        ],
+        feats: [1, 2],
+        spells: [],
+        backstory: '',
+        effects: [1, 2]
+    });
+    characterService.setCharacter($scope.character);
+
+    $scope.logCharacter = function() {
+        console.log($scope.character);
+    };
+}
